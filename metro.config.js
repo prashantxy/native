b45544db4d19ae32ef17021ff47c8732494bc400
Prashant Dubey <pdubey1924@gmail.com>
2025-01-26 12:28:26 +0530

    first commit

---
+++ metro.config.js
@@ -0,0 +1,17 @@
+/**
+ * Metro 
+ * https://github.com/react-native
+ *
+ * @format
+ */
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
